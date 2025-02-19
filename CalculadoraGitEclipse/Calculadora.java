8faddf00024735a2feb071d4c214c3cc95f13718
xlilith279 <xlilith279@gmail.com>
2025-02-19 14:00:37 +0100

    Funciones de resta y multiplicación añadidas

--- CalculadoraGitEclipse/Calculadora.java
+++ CalculadoraGitEclipse/Calculadora.java
@@ -1,3 +1,4 @@
+import java.sql.SQLIntegrityConstraintViolationException;
 
 public class Calculadora {
 
@@ -5,10 +6,20 @@ public class Calculadora {
 		return a + b;
 	}
 	
+	public int restar(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+	
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
-		
+
 		System.out.println("Resultado de la suma: " + calc.sumar(5, 3));
+		System.out.println("Resultado de la resta: " + calc.restar(5, 3));
+		System.out.println("Resultado de la multiplicacion: " + calc.multiplicar(5, 3));
 	}
 
 }
